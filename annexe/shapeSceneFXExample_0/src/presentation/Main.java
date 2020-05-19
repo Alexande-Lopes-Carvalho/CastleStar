231c4d88131beb0e909acad8eb90a38593d370b9
AlexandreLopesCarvalho <56762232+AlexandreLopesCarvalho@users.noreply.github.com>
2020-05-19 19:45:45 +0200

    Ajout de commentaire pour l'exemple en annexe

--- annexe/shapeSceneFXExample_0/src/presentation/Main.java
+++ annexe/shapeSceneFXExample_0/src/presentation/Main.java
@@ -8,8 +8,19 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage stage) {
+		/*
+		 *On empeche la fenetre de pouvoir etre redimensionable 
+		 */
 		stage.setResizable(false);
 		stage.sizeToScene();
+		/*
+		 * On crée une Scene contenant un Canvas et on l'ajoute dans notre stage
+		 * Dans le constructeur de FXCanvas, la fonction setup de notre EventHandler (ici une instance de MainEventHandler)
+		 * va etre appeler 
+		 * 
+		 * Puis a un rythme régulier les procedures calcEvent(long timePassed) et render() seront appeler dans notre EventHandler (ici l'instance de MainEventHandler)
+		 * 
+		 */
 		stage.setScene(new FXCanvas(new MainEventHandler()));
 		stage.show();
 	}
--- annexe/shapeSceneFXExample_0/src/presentation/PresColoredRectangle.java
+++ annexe/shapeSceneFXExample_0/src/presentation/PresColoredRectangle.java
@@ -10,23 +10,43 @@ public class PresColoredRectangle extends EventHandler {
 	private CtrlColoredRectangle c;
 	private Paint p;
 	private Point coord, dim;
+
 	public PresColoredRectangle(CtrlColoredRectangle _c) {
 		c = _c;
 		addEvent(new NewColorEvent().in(NewColorEvent.PERIOD));
 	}
-	
+
 	public void setPaint(Paint _p) {
 		p = _p;
 	}
-	
+
+	/*
+	 * Ici on ne fait rien dans notre methode calc, la sous classe de EventHandler
+	 * ne nous demandant pas de traitement particulier
+	 * 
+	 * Par contre dans des sous classe d'EventHandler nous demandant par exemple le deplacement d'un personnage ou autres ...
+	 * Le traitement de la requete a notre classe Controle se ferait ici
+	 */
 	public void calc(long timePassed) {
 	}
-	
+	/*
+	 * On souhaite afficher un rectangle avec sa couleur au coordonné et au dimension adéquat
+	 */
 	public void render() {
+		/*
+		 * on passe le fond des forme geometrique a afficher de la couleur p
+		 */
 		fill(p);
+		/*
+		 * on affiche notre rectangle
+		 * coord.getX() : coordonné en X
+		 * coord.getY() : coordonné en Y
+		 * dim.getX() : longeur (width)
+		 * dim.getY() : largeur (height)
+		 */
 		rect(coord, dim);
 	}
-	
+
 	public Point getCoord() {
 		return coord;
 	}
@@ -42,16 +62,27 @@ public class PresColoredRectangle extends EventHandler {
 	public void setDimension(Point dim) {
 		this.dim = dim;
 	}
-
+	/*
+	 * si on clique alors on change la couleur du rectangle
+	 */
 	public void mousePressed() {
 		c.newColor();
 	}
-	
+	/*
+	 * On crée un evenement qui va appeler toute les 1000 ms la methode newColor() de notre CtrlColoredRectangle c
+	 */
 	public class NewColorEvent implements Event {
 		public static final int PERIOD = 1000;
+
 		@Override
 		public void handleEvent() {
+			/*
+			 * Ici on appel la fonction
+			 */
 			c.newColor();
+			/*
+			 * On ajoute un nouvel evenement pour changer de couleur dans 1000 ms
+			 */
 			addEvent(new NewColorEvent().in(PERIOD));
 		}
 	}
