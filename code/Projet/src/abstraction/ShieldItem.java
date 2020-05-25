bf3e6d4ebc09cb870bd94d44aa00ac2d8bce4409
Antoine Delay <antoine.delay@eisti.fr>
2020-05-25 22:46:06 +0200

    Update ShieldItem.java

--- code/Projet/src/abstraction/ShieldItem.java
+++ code/Projet/src/abstraction/ShieldItem.java
@@ -5,8 +5,8 @@ public class ShieldItem extends Item{
 	public ShieldItem(Point coord) {
 		super(coord);
 	}
-	public void pickUpBy(Player player) {
-		player.setHasShield(true);
+	public void pickUpBy(CtrlPlayer ctrlPlayer) {
+		ctrlPlayer().equip(new CtrlShield());
 	}
 
 }
